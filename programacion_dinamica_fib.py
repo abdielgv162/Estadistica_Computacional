b293b211c4bdb0aff094669c2a8b147008ccabfd
Abdiel Guerrero <jabdi16@outlook.es>
2020-09-28 01:07:25 -0500

    fibonacci dinamica

---
+++ programacion_dinamica_fib.py
@@ -0,0 +1,25 @@
+import sys
+
+def fibonacci_recursivo(n):
+    if n == 0 or n == 1:
+        return 1
+
+    return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
+
+def fibonacci_dinamico(n, memoria={}):
+    if n == 0 or n == 1 :
+        return 1
+    try:
+        return memoria[n]
+    except KeyError:
+        resultado = fibonacci_dinamico(n-1, memoria) + fibonacci_dinamico(n-2, memoria)
+        memoria[n] = resultado
+
+        return resultado
+
+if __name__ == "__main__":
+    sys.setrecursionlimit(10000)
+    n = int(input("Escoge un numero: "))
+    # resultado = fibonacci_recursivo(n)
+    resultado = fibonacci_dinamico(n)
+    print(resultado)
